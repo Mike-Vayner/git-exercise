c0d4d1990d1d0b9f3d4ad4277492a2bc5eb5deed
Mike-Vayner <113300443+Mike-Vayner@users.noreply.github.com>
2023-10-17 10:39:27 +0000

    Update DAC to be more flexible

--- dac.py
+++ dac.py
@@ -12,6 +12,8 @@ class DAC:
     """
 
     analog: float
+    max_voltage: float
+    digits: int
 
     @property
     def digital(self) -> int:
@@ -20,18 +22,22 @@ class DAC:
         To print this number in a binary format, use the `0=10b` format
         in an f-string or `str.format()`."""
 
-        return round(self.analog * 204.8)
+        return round(self.analog * (2**self.digits / self.max_voltage))
 
     @digital.setter
     def digital(self, value: int) -> None:
         if value > 0b1111111111:
             raise ValueError(
-                f"voltage must be between 0 and 0x1111111111 (is {value:0=10b})"
+                f"voltage must be between 0 and 0x{2 ** self.digits:0=10b} (is {value:0=10b})"
             )
-        self.analog = value / 204.8
+        self.analog = value / (2**self.digits / self.max_voltage)
 
 
-def create_dac(voltage: float) -> DAC:
+def create_dac(voltage: float, max_voltage: float = 5, digits: int = 10) -> DAC:
     if voltage < 0 or voltage > 5:
         raise ValueError(f"voltage must be between 0 and 5 (is {voltage})")
-    return DAC(voltage)
+    if max_voltage < 0:
+        raise ValueError("max voltage cannot be below 0")
+    if digits <= 0:
+        raise ValueError("cannot represent a number with 0 or less digits")
+    return DAC(voltage, max_voltage, digits)
